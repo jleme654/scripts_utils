f0c77258d97c80575f0466edc258ebece3cca153
Julio Leme <julioromao3@gmail.com>
2018-06-26 11:50:27 -0300

    Ajustes para data_hora_criacao e idDevolucaoOMS em INSERT

--- src/br/com/scripts/homolog/Main_HR_B2C_ATENDIMENTO.java
+++ src/br/com/scripts/homolog/Main_HR_B2C_ATENDIMENTO.java
@@ -31,7 +31,7 @@ public class Main_HR_B2C_ATENDIMENTO {
 		String s = "INSERT INTO LINX_DB_TESTE_OMNI.dbo.HR_B2C_ATENDIMENTO (ID_B2C_ATENDIMENTO, CODIGO_TIPO_ATENDIMENTO, CODIGO_FILIAL, STATUS_SAP, CODIGO_CLIENTE, PEDIDO, PEDIDO_DEVOLUCAO, VALOR_ATENDIMENTO, CODIGO_VOUCHER, DATA_ATIVACAO_VOUCHER, PROTOCOLO_SAC, CODIGO_REFERENCIA_DEVOLUCAO, SITUACAO_ATUAL, MOTIVO_CANCELAMENTO, CODIGO_POSTAGEM, SERIE_NF_ENTRADA, NUMERO_NF_ENTRADA, DIGITACAO, PEDIDO_ENTRADA, DESC_MOTIVO_TROCA_DEVOLUCAO, ID_HYBRIS_PEDIDO_DEVOLUCAO, HR_ISO_CAUSA, SAP_FORCA_REFERENCIA_DEVOLUCAO, FATURAR_CONTRA_CENTRO_CUSTO, HR_DATA_HORA_CRIACAO, ID_DEVOLUCAO_OMS, CODIGO_MODALIDADE_RESSARCIMENTO, VALOR_FRETE_RESSARCIDO, PEDIDO_OMS_ORIGINAL, CODIGO_ENTREGA_OMS_ORIGINAL, STATUS_SAP_ADIANTAMENTO, SAP_ORDEM_DEVOLUCAO, SAP_FATURA_DEVOLUCAO, SAP_REMESSA_DEVOLUCAO, EMISSAO_NF_ENTRADA, ULTIMA_MODIFICACAO)", 
 			   s2 = "VALUES(",//'001', '",
 			   s3 = ", '",//6', 
-		       s4 = "', '84', 0, '0009', 0, 0, 0, '', '', '', '', '100', 4, '', '', '', '', 0, '', '', 0, 0, 0, '', '",// '9001', '', 0, '', '', 0, '', '', '', '', '')"";
+		       s4 = "', '84', 0, '0009', 0, 0, 0, '', '', '', '', '100', 4, '', '', '', '', 0, '', '', 0, 0, 0, '",// '9001', '', 0, '', '', 0, '', '', '', '', '')"";
 		       s5 = "' ,'', 0, '', '', 0, '', '', '', '', '')";
 		
 		List<String> listaPedidos = verifyListaPedidos(codigoTipoAtendimento);
@@ -44,7 +44,7 @@ public class Main_HR_B2C_ATENDIMENTO {
 			sb.append(s3);
 			sb.append(codigoTipoAtendimento);
 			sb.append(s4);
-			sb.append(String.valueOf(idDevolucaoOMS++));
+			sb.append(Pedidos_OMNI_Utils.getDataAtual()).append("', '").append(String.valueOf(idDevolucaoOMS++));
 		    sb.append(s5);
 			
 			System.out.println(sb.toString());
