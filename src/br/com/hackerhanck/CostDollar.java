04580f12451f0252941415b073bcb21892b40492
Julio Leme <julioromao3@gmail.com>
2018-09-19 16:35:04 -0300

    Challenge Cost Dollar - v-1.1

--- src/br/com/hackerhanck/CostDollar.java
+++ src/br/com/hackerhanck/CostDollar.java
@@ -19,4 +19,21 @@ public class CostDollar {
 		String s = "abcd";
 		System.out.println(stringConstruction(s));
 	}
+	
+	/*
+	 * Solution II
+	 * 
+	 * public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int n = in.nextInt();
+	    for(int a0 = 0; a0 < n; a0++){
+	        String s = in.next();
+	        Set<Character> aiseHi = new HashSet<>();
+	        char[] c = s.toCharArray();
+	        for(char ch: c) {
+	            aiseHi.add(ch);
+	        }
+	        System.out.println(aiseHi.size());
+	    }
+	   }*/
 }
